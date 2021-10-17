e70acd5ab029f59c0bf88f1d2cf8d7cd037bfb14
Jorge Jacques <jjacquesf@gmail.com>
2021-10-16 20:27:11 -0500

    improve default page

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// configure routes
 app.use('/',      require('./routes/index'));
 app.use('/users', require('./routes/users'));
 app.use('/docs',  swaggerUi.serve, swaggerUi.setup(swaggerSpec));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'JSONPlaceholder Express API' });
+  res.render('index', { title: 'Express API' });
 });
 
 module.exports = router;
